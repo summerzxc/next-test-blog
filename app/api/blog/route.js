7d4cb406c4ebbd9920c52de4ed87c820f5d3f4c4
summerzxc <cell.unobtainable.88@gmail.com>
2024-03-28 17:02:00 +0800

    bug fix

---
+++ app/api/blog/route.js
@@ -0,0 +1,37 @@
+import connectDB from "@/lib/db";
+import category from "@/model/scheme";
+import { NextResponse } from "next/server";
+import Cors from 'cors';
+import initMiddleware from '@/lib/init-middleware';
+
+// Initialize the CORS middleware
+const cors = initMiddleware(
+  Cors({
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed HTTP methods
+    origin: '*' // Set the allowed origin (change this to restrict to specific origins)
+  })
+);
+
+export async function GET(req, res) {
+  try {
+    await connectDB();
+    const categories = await category.find();
+    return NextResponse.json({ categories });
+  } catch (error) {
+    return NextResponse.json({ message: error.message });
+  }
+}
+
+export async function POST(req, res) {
+  await cors(req, res); // Apply CORS middleware
+  try {
+    const { title, description } = await req.json();
+    await connectDB();
+    await category.create({ title, description });
+    return NextResponse.json({ message: "success", category });
+  } catch (error) {
+    return NextResponse.json({ message: error.message });
+  }
+}
+
+
